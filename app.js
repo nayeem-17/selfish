bd9d4e9d466e62b7fdd048137d6d71cfb2622124
parasite <nasa.28apr@gmail.com>
2020-11-13 19:14:58 +0600

    added controllers for crud operations

--- app.js
+++ app.js
@@ -2,28 +2,32 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const morgan = require('morgan')
-const { addData } = require('./controllers/AddData')
+const { getTuiTionInfo, createTuiTionInfo, updateTuiTionInfo, deleteTuiTionInfo } = require('./controllers/TuitionInfoControllers')
 require('dotenv').config()
 
 const port = process.env.PORT || 3000
-console.log(process.env.MONGO_CLOUD)
 
 const app = express()
 app.use(morgan('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
-mongoose.connect(process.env.MONGO_CLOUD, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).catch(e => {
-    console.log("Totally Fucked Up With Connecting Database , Man!")
-    console.log(e)
-}).then(con => {
-    console.log("Connection Established")
-})
+mongoose
+    .connect(process.env.MONGO_CLOUD, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(con => {
+        console.log("Database Connection Established")
+    })
+    .catch(e => {
+        console.log("Totally Fucked Up With Connecting Database , Man!")
+        console.log(e)
+    })
 
 app.get('/', (req, res) => res.send('Hello World!'))
-app.get('/test', addData)
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/getfulldata/:id', getTuiTionInfo)
+app.get('/createtuition', createTuiTionInfo)
+app.patch('/updatetuitionInfo/:id', updateTuiTionInfo)
+app.delete('/deletetuitionInfo/:id', deleteTuiTionInfo)
+app.listen(port, () => console.log(`Example app listening on port ${port} !`))
---
+++ controllers/TuitionInfoControllers.js
@@ -0,0 +1,63 @@
+const { TuitionInfo } = require("../models/MainModel")
+
+exports.getTuiTionInfo = async (req, res) => {
+    try {
+        id = req.params["id"]
+        console.log(req.params)
+        const data = await TuitionInfo.findById(id)
+        res.status(200).json({
+            data: data,
+            message: "OK"
+        })
+    } catch (error) {
+        res.status(404).json({
+            message: "Error occured ! Read the documentation carefully",
+            error: error
+        })
+    }
+}
+exports.createTuiTionInfo = async (req, res) => {
+    try {
+        const data = req.body
+        const newData = await TuitionInfo.create(data)
+        res.status(200).json({
+            message: "OK",
+            id: newData["_id"]
+        })
+    } catch (error) {
+        res.status(404).json({
+            message: "Error occured ! Read the documentation carefully",
+            error: error
+        })
+    }
+}
+exports.updateTuiTionInfo = async (req, res) => {
+    try {
+        const data = req.body
+        const updated = await TuitionInfo.findByIdAndUpdate(req.params.id, data, {
+            new: true
+        })
+        res.status(200).json({
+            updated: updated,
+            message: "OK"
+        })
+    } catch (error) {
+        res.status(404).json({
+            message: "Error occured ! Read the documentation carefully",
+            error: error
+        })
+    }
+}
+exports.deleteTuiTionInfo = async (req, res) => {
+    try {
+        await TuitionInfo.findByIdAndDelete(req.params.id)
+        res.status(200).json({
+            message: "OK"
+        })
+    } catch (error) {
+        res.status(404).json({
+            message: "Error occured ! Read the documentation carefully",
+            error: error
+        })
+    }
+}
