06cb7e3df1485fc520392a87d55716cfe0bdb0c0
Nayeem-17 <nasa.28apr@gmail.com>
2021-02-04 15:11:55 +0600

    added auto-documentation  and docker config file

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const morgan = require('morgan')
-const { getTuiTionInfo, createTuiTionInfo, updateTuiTionInfo, deleteTuiTionInfo } = require('./controllers/TuitionInfoControllers')
-require('dotenv').config()
+require('dotenv').config();
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+const { getTuiTionInfo, createTuiTionInfo, updateTuiTionInfo, deleteTuiTionInfo } = require('./controllers/TuitionInfoControllers');
 
 const port = process.env.PORT || 3000
 
@@ -25,10 +27,106 @@ mongoose
         console.log(e)
     })
 
+/**   
+* @swagger   
+*  /:
+*      get:
+*        responses:
+*          "200":
+*            description: OK
+*/
 app.get('/', (req, res) => res.send('Hello World!'))
+
+/**
+ * @swagger
+ * /getfulldata:   
+ *  get:
+ *   description: To access all data from servers
+ *   responses:
+ *       '200':
+ *           description: A successfull response.
+ */
+
 app.get('/getfulldata', getTuiTionInfo)
+/**
+ *  @swagger
+  * /createtuition:
+  *   post:
+  *    description: Update the data of a particuler id
+  *    requestBody:
+  *      required: true
+  *      content:
+  *        application/json:
+  *          schema:
+  *            $ref: "#/components/schemas/tuitionSchema"
+  *    responses:
+  *      "200":
+  *        description: A successfull response. 
+  * */
 app.post('/createtuition', createTuiTionInfo)
+/**
+ * @swagger
+ * /updatetuitioninfo/{id}:   
+ *  patch:
+ *   description: Update the data of a particuler id
+ *   parameters:
+ *    - in: path
+ *      name: id
+ *      schema:
+ *          type: string
+ *      required: yes
+ *      example: "600aafc8c52663128624ebc6"
+ *      description: id of the tuition object
+ *   requestBody:
+ *      required: true
+ *      content:
+ *         application/json:  
+ *           schema:
+ *              $ref: '#/components/schemas/tuitionSchema'
+ *   responses:
+ *       '200':
+ *           description: A successfull response.
+ */
 app.patch('/updatetuitioninfo/:id', updateTuiTionInfo)
+/**
+ * @swagger
+ * /deletetuitioninfo/{id}:   
+ *  delete:
+ *   description: Delete the data of a particuler id
+ *   parameters:
+ *    - in: path
+ *      name: id
+ *      schema:
+ *          type: string
+ *      required: yes
+ *      example: "600aafc8c52663128624ebc6"
+ *      description: id of the tuition object
+ *   responses:
+ *       '200':
+ *           description: A successfull response.
+ */
 app.delete('/deletetuitioninfo/:id', deleteTuiTionInfo)
+
+const options = {
+    swaggerDefinition: {
+        openapi: "3.0.1",
+        info: {
+            title: "Backend API",
+            description: "THis is a simple api created by using Express framework .Swagger is used auto-documetation.",
+            version: "1.0.0",
+            servers: [`http://localhost:${process.env.PORT}`],
+            contact: {
+                name: "dude"
+            }
+        },
+    },
+    apis: [
+        "*.js",
+        "./models/*.js"
+    ]
+
+};
+const swaggerDocs = swaggerJsdoc(options)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 
 app.listen(port, () => console.log(`Example app listening on port ${port} !`))
--- models/MainModel.js
+++ models/MainModel.js
@@ -19,3 +19,92 @@ const mainSchema = new mongoose.Schema({
     }
 })
 exports.TuitionInfo = mongoose.model("TuitionInfo", mainSchema)
+
+/**
+* @swagger
+* components:
+*   schemas:
+*     monthSchema:
+*       type: object
+*       properties:
+*         monthNo:
+*           type: integer
+*           example: 09
+*         startDate:
+*           type: string
+*           format: date
+*           example: 2021-09-11
+*         endDate:
+*           type: string
+*           format: date
+*           example: 2021-09-11
+*         dayCount:
+*           type: integer
+*           example: 09
+*         activeDates:
+*           type: array
+*           items:
+*             type: string
+*             format: date
+*           example: [2021-09-11, 2021-09-11, 2021-09-11]
+*     contactSchema:
+*       type: object
+*       properties:
+*         relation:
+*           type: string
+*           example: papa
+*         phone:
+*           type: integer
+*           example: 8801764223436
+*     paymentSchema:
+*       type: object
+*       properties:
+*         date:
+*           type: string
+*           format: date
+*           example: 2021-09-11
+*         amount:
+*           type: integer
+*           example: 12000
+*         note:
+*           type: string
+*           example: yo bruh
+*     tuitionSchema:
+*       type: object
+*       properties:
+*         studentName:
+*           type: string
+*           example: nayeem
+*         salaryPerMonth:
+*           type: integer
+*           example: 10000
+*         address:
+*           type: string
+*           example: polashi
+*         isRunning:
+*           type: boolean
+*           example: true
+*         totalEarned:
+*           type: integer
+*           example: 25000
+*         dueAmount:
+*           type: integer
+*           example: 7500
+*         contacts:
+*           type: array
+*           items:
+*             $ref: "#/components/schemas/contactSchema"
+*         payements:
+*           type: array
+*           items:
+*             $ref: "#/components/schemas/paymentSchema"
+*         weekDay:
+*           type: array
+*           items:
+*             type: string
+*             format: date
+*         months:
+*           type: array
+*           items:
+*             $ref: "#/components/schemas/monthSchema"
+*/
